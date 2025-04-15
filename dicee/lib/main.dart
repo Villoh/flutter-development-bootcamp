c7e1d6b36a1dbae61429af514df9c21eb20ac5ff
Villoh <mikelvillotatic@gmail.com>
2025-04-16 00:15:55 +0200

    Complete Dicee app layout and add dice images

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -14,8 +14,9 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
-  const DicePage({super.key});
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -26,20 +27,31 @@ class DicePage extends StatelessWidget {
             child: TextButton(
               style: TextButton.styleFrom(padding: const EdgeInsets.all(16.0)),
               onPressed: () {
-                Image.asset('assets/images/dice${Random().nextInt(6) + 1}.png');
+                setState(() {
+                  leftDice = Random().nextInt(6) + 1;
+                });
               },
-              child: Image.asset('assets/images/dice1.png'),
+              child: Image.asset('assets/images/dice$leftDice.png'),
             ),
           ),
           Expanded(
             child: TextButton(
               style: TextButton.styleFrom(padding: const EdgeInsets.all(16.0)),
-              onPressed: () {},
-              child: Image.asset('assets/images/dice1.png'),
+              onPressed: () {
+                setState(() {
+                  rightDice = Random().nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('assets/images/dice$rightDice.png'),
             ),
           ),
         ],
       ),
     );
   }
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _DicePageState();
 }
